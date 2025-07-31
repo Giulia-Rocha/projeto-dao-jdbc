62f9e15f52800fa5410083b571e9fd0ca982a119
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-07-31 12:31:56 -0300

    findById implementation

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -1,4 +1,72 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class DB {
+    private static Connection conn = null;
+
+    //gera a conexão com o db
+    public static Connection getConnection() {
+        if(conn == null){
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            }catch(SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    //fecha a conexão
+    public static void closeConnection(){
+        if(conn != null){
+            try{
+                conn.close();
+            }catch(SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    //fecha um statement
+    public static void closeStatement(Statement stmt){
+        if(stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    // fecha um resultset
+
+    public static void closeResultSet(ResultSet rs){
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    // busca as propriedades do banco no arquivo db.properties
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
 }
